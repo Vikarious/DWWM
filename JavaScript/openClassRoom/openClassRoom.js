4c6bd4cf370cf2da8a6acbf04d2176fedcab7f9b
Vikarious <36402524+Vikarious@users.noreply.github.com>
2022-01-07 10:09:21 +0100

    +

---
+++ JavaScript/openClassRoom/openClassRoom.js
@@ -0,0 +1,29 @@
+/*
+let myEpisodes = {
+    title: 'Arcane',
+    duration: 55,
+    hasBeenWatched: true,
+};
+let episodeTitle = myEpisodes.title;
+let episodeDuration = myEpisodes.duration;
+let episodeHasBeenWatched = myEpisodes.hasBeenWatched;
+*/
+
+//
+
+/*
+class Episodes {
+    constructor(title, duration, hasBeenWatched) {
+        this.title = title;
+        this.duration = duration;
+        this.hasBeenWatched = hasBeenWatched;
+    }
+}
+
+let myEpisodes = new Episodes('arcane', 55, true);
+let myEpisodes = new Episodes('Bojack Horseman', 60, true);
+let myEpisodes = new Episodes('JoJo', 25, false);
+*/
+
+//
+
