c099eceef9077218c132e325a7aee6c1cc252a5e
Vikarious <36402524+Vikarious@users.noreply.github.com>
2022-01-10 15:03:18 +0100

    Python

--- Séances Javascript/Apprendre à programmer/Python/Py Exemple/Exemple1/test1.py
+++ Séances Javascript/Apprendre à programmer/Python/Py Exemple/Exemple1/test1.py
@@ -23,3 +23,39 @@ chaine = "Et voila du texte"
 chaine1 = ' Nous l\'avons'
 chaine3 = ' \"reparee\"'
 print(chaine + chaine1 + chaine3)
+
+# reponse = input() # Une ligne vide apparait et attends un input de l'utilisateur
+def age():
+    age = int(input("Quel est votre age?"))
+    print('Vous avez ' + str(age) + ' ans')
+
+    if age >= 16 and age < 100 :
+        print('Vous avez plus de 16 ans')
+    elif age > 100 :
+        print('Tu te moquerais pas de moi?')
+    else :
+        print('Vous avez moins de 16 ans')
+
+
+# print(type(age))
+
+a = 3
+print(type(a))
+b = 8.2
+print(type(b))
+chaine = 'Au revoir'
+print(type(chaine))
+vrai = True
+print(type(vrai))
+
+print(7 > 5 > 1)
+print(7 > 5 < 9 != 10)
+
+n = 0
+while n <  10 :
+    n += 1
+    if n % 2 != 0 :
+        print(str(n) + ' est impaire')
+    else :
+        print(str(n) + ' est paire')
+    
---
+++ Séances Javascript/Apprendre à programmer/Python/Py Exemple/Exemple1/test2.py
@@ -0,0 +1,6 @@
+a = input("Entrez un entier SVP: ")
+print(str(a))
+b = 0
+
+a, b = b, a
+print(str(b))
