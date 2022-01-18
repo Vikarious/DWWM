31b5d71ef4bffcfd4882381045642491262343dc
Vikarious <36402524+Vikarious@users.noreply.github.com>
2022-01-18 16:45:52 +0100

    +

---
+++ Séances Javascript/Javascript/sceance2_javascript/essai1/js/main.js
@@ -0,0 +1,32 @@
+// let number = 0;
+// number = number + 1;
+// alert(number);
+
+// number--;
+// alert(number);
+
+// let number = 0;
+// let output = number++;
+
+// alert(number);
+// alert(output);
+
+// let number = 0;
+// let output = ++number;
+
+// alert(number);
+// alert(output);
+
+// let number = 0;
+// while(number < 10) {
+//     alert(number);
+//     number++;
+// }
+// alert(number);
+
+// do {
+//     instruction1;
+//     instruction2;
+//     instruction3;
+// }
+
