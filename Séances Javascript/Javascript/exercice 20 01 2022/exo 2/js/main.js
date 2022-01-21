9f273ea16d4ddea99597f555ab078d3f0ada0d47
Vikarious <fialaire.arnaud@free.fr>
2022-01-21 14:50:34 +0100

    +

--- Séances Javascript/Javascript/exercice 20 01 2022/exo 2/js/main.js
+++ Séances Javascript/Javascript/exercice 20 01 2022/exo 2/js/main.js
@@ -1,9 +1,23 @@
 let age = parseInt(prompt('Votre age svp: '));
 
-while(age < 0) {
+while(age <= 0) {
     age = prompt('erreur: Resaisissez votre age SVP: ');
-} if(age <= 17) {
+} 
+if(age <= 17) {
     alert('Vous etes trop jeune !');
-} else {
+} 
+else {
     alert('Vous avez ' + age);
 }
+
+// let age_errone;
+
+// if(age < 0) {
+//     while(age < 0) {
+//         age_errone = parseInt(prompt('erreur: ressaisissez votre age: '));
+//         age = age_errone;
+//     }
+//     alert('Vous avez ' + age);
+// } else {
+//     alert('Vous avez ' + age);
+// }
