b24ef7f5603599e38ec3f4ba8d6f69b8681d4d53
Vikarious <fialaire.arnaud@free.fr>
2022-01-30 17:00:16 +0100

    exercice 28 01 2022

---
+++ Séances Javascript/Javascript/exercice 28 01 2022/js/main.js
@@ -0,0 +1,7 @@
+﻿$('.dropDownOption').click(function() {
+    var numberTables = parseInt($(this).html()),
+        tableHTML = "<table class='table'><tr><td>Foo</td><td>Bar</td><tr><td>Spam</td><td>Ham</td></tr></table>";
+    for (var i = 0; i < numberTables; i++) {
+        $('body').append(tableHTML);
+    }
+});
